61c6319c11a719d160cf82a84fc9d3594913052c
michonchy <mintan0222@gmail.com>
2021-11-14 23:29:56 +0000

    四則演算

--- quiz005.py
+++ quiz005.py
@@ -1,8 +1,10 @@
 def keisann(x,y):
- return x+y,x-y,x*y,x/y,x%y
+    x,y=int(x),int(y)
+    return x+y,x-y,x*y,x/y,x%y
 
-
-print(keisann(12, 3))
+A=input("What is your number 1 >>\n")
+B=input("What is your number 2 >>\n")
+print(keisann(A,B))
 
 
 
