4d61d2b09aeb72a7061fb3ef1e34444cb8b1bb1a
michonchy <mintan0222@gmail.com>
2021-11-15 11:07:14 +0000

    場合分けの表示　01

--- quiz006.py
+++ quiz006.py
@@ -1,7 +1,8 @@
-x=0
-if x==0:
-
-
-    print("zero")
-
-
+def keisann(x):
+    x=int(x)
+    if x==0:
+        return "zero"
+    else:
+        return "x"
+n=input("What is your number >>\n")
+print(keisann(n))
