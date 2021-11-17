013fa6777ee8f97afca698eddce98895c5078d7b
michonchy <mintan0222@gmail.com>
2021-11-17 23:33:11 +0000

    テストコードの追加

--- quiz003.py
+++ quiz003.py
@@ -1,7 +1,13 @@
+def is_number(x):
+    if not x.isnumeric() and not x.startswith("-"):
+        return False
+    return True
 
 def number(x):
+    if is_number (x):
+        return "整数値を入力してください。"
     return int(x)
 
-
-n = input("What is your number >>\n")
-print(number(n))
+if __name__ == '__main__':
+    n = input("What is your number >>\n")
+    print(number(n))
---
+++ test_quiz003.py
@@ -0,0 +1,6 @@
+import quiz003
+
+def test_number():
+    assert quiz003.number("123") == 123
+
+
