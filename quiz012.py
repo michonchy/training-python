489c527ee38ade73bf3f3c99fefbe674795ed8fe
michonchy <mintan0222@gmail.com>
2021-11-21 23:29:59 +0000

    入力させた回数だけhello waord を表示する

---
+++ quiz012.py
@@ -0,0 +1,10 @@
+hello_world_set = ""
+n = input("How many times do yo indicate?>>\n")
+n = int(n)
+for index in range(n):
+    hello_world_set +="Hello World!\n"
+print(hello_world_set)
+# [出力結果]
+# 0
+# 1
+# 2
