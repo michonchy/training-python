d3c0bc1b9430a6a09d8f995ed7c608b9e5afd17b
michonchy <mintan0222@gmail.com>
2021-11-22 23:55:39 +0000

    自作例外（関数の使い方わかったよ）

--- quiz012.py
+++ quiz012.py
@@ -1,15 +1,17 @@
-hello_world_set = ""
-n = input("How many times do yo indicate?>>\n")
 class InvalidError(Exception):
     pass
-    def is_number(n):
-        if not n.isdigit():
-            return False
-        return True
-        
-n == int(n)
-for index in range(n):
-    raise InvalidError("整数値を入力してください。")
-    hello_world_set +="Hello World!\n"
+def is_number(x):
+    if not x.isdigit():
+        return False
+    return True
+def number(x):
+    if not is_number(x):
+        raise InvalidError("整数値を入力してください。")
+    return int(x)
 
+hello_world_set = ""
+a = input("How many times do yo indicate?>>\n") 
+n = number(a)      
+for index in range(n):
+    hello_world_set +="Hello World!\n"
 print(hello_world_set)
