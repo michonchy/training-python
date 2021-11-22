ca792ec7a843582ce1a6b86892702b089df0efd0
michonchy <mintan0222@gmail.com>
2021-11-22 13:02:41 +0000

    自作例外（途中）

--- quiz012.py
+++ quiz012.py
@@ -1,10 +1,15 @@
 hello_world_set = ""
 n = input("How many times do yo indicate?>>\n")
-n = int(n)
+class InvalidError(Exception):
+    pass
+    def is_number(n):
+        if not n.isdigit():
+            return False
+        return True
+        
+n == int(n)
 for index in range(n):
+    raise InvalidError("整数値を入力してください。")
     hello_world_set +="Hello World!\n"
+
 print(hello_world_set)
-# [出力結果]
-# 0
-# 1
-# 2
