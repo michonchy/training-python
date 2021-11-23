23089bf1c86bc6a2ee0d1a3ca3ebf715adf2b442
michonchy <mintan0222@gmail.com>
2021-11-23 12:33:51 +0000

    例外の追加

--- quiz013.py
+++ quiz013.py
@@ -1,4 +1,16 @@
-n = input("How many times do yo indicate?>>\n")
-n = int(n)
+class InvalidError(Exception):
+    pass
+def is_number(x):
+    if not x.isdigit():
+        return False
+    return True
+def number(x):
+    if not is_number(x):
+        raise InvalidError("整数値を入力してください。")
+    return int(x)
+
+a = input("How many times do yo indicate?>>\n")
+n = number(a)
 for number in range(0,n+1):
     print(number)
+
