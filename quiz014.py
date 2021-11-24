d6e538e5f119e616cbb746ebf78f1e4a681d8382
michonchy <mintan0222@gmail.com>
2021-11-24 09:35:48 +0000

    カウントダウン

---
+++ quiz014.py
@@ -0,0 +1,17 @@
+class InvalidError(Exception):
+    pass
+def is_number(x):
+    if not x.isdigit():
+        return False
+    return True
+def number(x):
+    if not is_number(x):
+        raise InvalidError("整数値を入力してください。")
+    return int(x)
+
+if __name__ == '__main__':
+    a = input("How many times do yo indicate?>>\n")
+    n = number(a)
+    for number in range(n,-1,-1):
+        print(number)
+
