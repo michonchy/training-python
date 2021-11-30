903f44d027c999bcbdf2ab7a395b0f43215aee85
michonchy <mintan0222@gmail.com>
2021-11-30 09:21:33 +0000

    pytest16

--- quiz016.py
+++ quiz016.py
@@ -1,6 +1,7 @@
 num = None
 while num !=0:
     print("numの今の値は" + str(num))
-    num = input("What is your number >>\n")
-    num = int(num)
+    if __name__ == '__main__':
+        num = input("What is your number >>\n")
+        num = int(num)
 print("End")
---
+++ test_quiz016.py
@@ -0,0 +1,9 @@
+import quiz016
+
+def test_number():
+    assert quiz016.is_number("0") 
+    return "End"
+
+
+
+
