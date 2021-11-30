2d23877febf890f6aa81f4c0042514d31b9cef98
michonchy <mintan0222@gmail.com>
2021-12-01 08:31:54 +0900

    pytestできるようになったよ

--- quiz016.py
+++ quiz016.py
@@ -1,7 +1,9 @@
-num = None
-while num !=0:
-    print("numの今の値は" + str(num))
-    if __name__ == '__main__':
+
+if __name__ == '__main__':
+    num = None
+    while num !=0:
+        print("numの今の値は" + str(num))
+
         num = input("What is your number >>\n")
         num = int(num)
-print("End")
+    print("End")
