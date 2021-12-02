916b7ed8045ed75d9c987cd2caf27b61857d979f
michonchy <mintan0222@gmail.com>
2021-12-02 11:48:43 +0900

    問題の意味あっとるんかなー。。。

--- quiz017.py
+++ quiz017.py
@@ -1,2 +1,21 @@
-for index in range(10):
-    print(index)
+class InvalidError(Exception):
+    pass
+def is_number(x):
+    if not x.isdigit():
+        return False
+    return True
+def number(x):
+    if not is_number(x):
+        raise InvalidError("正の整数値を入力してください。")
+    return int(x)
+
+def number_array(n):
+    result = ""
+    for index in range(n):
+        result += str(index) + "\n"
+    return result 
+
+if __name__ == '__main__':
+    a = input("How many times do yo indicate?>>\n")
+    n = number(a)
+    print(number_array(n))
