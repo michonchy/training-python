a2553858b9c690802ddea28b554822a9fd9cbca2
michonchy <mintan0222@gmail.com>
2021-12-08 06:15:36 +0900

    pytest31

---
+++ test_quiz031.py
@@ -0,0 +1,22 @@
+import quiz031
+def test_number():
+    assert quiz031.is_number("2") 
+
+def test_number_with_few():
+    try:
+        quiz031.number("1.1")
+        assert False
+    except Exception as e:
+        assert str(e) == "整数値を入力してください。"
+
+def test_graph():
+    assert quiz031.graph(3) == " ***"
+    assert quiz031.graph(12) == " ***** ***** **"
+
+
+
+
+
+
+
+
