33b548e4cde71da3a86bdf2708d5513e44101fcf
michonchy <mintan0222@gmail.com>
2021-12-08 05:33:47 +0900

    棒グラフの作成

---
+++ quiz030.py
@@ -0,0 +1,31 @@
+# 整数値を入力させ、その個数だけ*を表示するプログラムを作成せよ。
+# 入力値が0以下の値の場合は何も書かなくてよい。
+class InvalidError(Exception):
+    pass
+def is_number(x):
+    if x.startswith("-"):
+        x = x[1:]
+    if not x.isdigit():
+        return False
+    return True
+
+def number(x):
+    if not is_number(x):
+        raise InvalidError("整数値を入力してください。")
+    return int(x)
+
+def graph(n):
+    if n <= 0:
+        return ""
+    ind_number = ""
+    for i in range(n):
+        ind_number += "*"
+    return ind_number
+
+
+if __name__ == '__main__':
+    a = input("How many times do yo indicate?>>\n") 
+    n = number(a)      
+    print(graph(n))
+
+
