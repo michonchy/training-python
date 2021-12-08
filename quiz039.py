fb20af4afb8c67c80b103b8cadbd900f0182b5af
michonchy <mintan0222@gmail.com>
2021-12-09 08:00:56 +0900

    pytest39

--- quiz039.py
+++ quiz039.py
@@ -1,7 +1,8 @@
 # {3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、
-# 最初は参照する要素番号を0とし、この参照する要素番号の配列要素の値を表示し、
-# 次にその配列要素の値を次の参照する要素番号とし、
-# ……を10回繰り返すプログラムを作成せよ。
+# 最初は参照する要素番号を0とする。
+# この参照する要素番号の配列要素の値から次の要素番号の配列要素の値を引いた値を表示し、
+# 参照する要素番号を1増やす。
+# この手順を9回繰り返すプログラムを作成せよ。
 class InvalidError(Exception):
     pass
 def is_number(x):
@@ -15,11 +16,12 @@ def number(x):
     return int(x)
 
 number_list = [3,7,0,8,4,1,9,6,5,2]
-def keisann(num):
-    for i in range(10):
-        num = number_list[num]
+def keisann(n):
+    num = 0
+    for i in range(n):
+        num = number_list[i]-number_list[i+1]
         print(num)
-    return None      
+    return None
  
 if __name__ == "__main__":
-    print(keisann(0))
+    print(keisann(9))
--- test_quiz039.py
+++ test_quiz039.py
@@ -1,16 +1,16 @@
-import quiz038
+import quiz039
 def test_number():
-    assert quiz038.is_number("2") 
+    assert quiz039.is_number("2") 
 
 def test_number_with_few():
     try:
-        quiz038.number("1.1")
+        quiz039.number("1.1")
         assert False
     except Exception as e:
         assert str(e) == "整数値を入力してください。"
 
 def test_keisann():
-    assert quiz038.keisann(0) == None
+    assert quiz039.keisann(0) == None
 
 
 
